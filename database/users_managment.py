697e08e5cbea83d3eee7d25abef0655847e40b66
Nikita Goncharov <nikitagoncarov657@gmail.com>
2024-01-12 16:08:41 +0200

    API CRUD model for users table

--- database/users_managment.py
+++ database/users_managment.py
@@ -4,15 +4,34 @@ from faker import Faker
 from sqlalchemy import text
 
 
-insert_user_query = text(
-    """insert into users (username, img, status, followed, country, city) 
-    values (:username, :img, :status, :followed, :country, :city)"""
-)
+# TODO: configure them in class
+def insert_user_query():
+    return text("""
+        insert into users (username, img, status, followed, country, city) 
+        values (:username, :img, :status, :followed, :country, :city)
+    """)
 
-select_all_users_query = text("select * from users")
 
-update_user = text("update users set username= , img= , ")
-delete_user = text("delete from users where id=")
+def select_all_users_query():
+    return text("select * from users")
+
+
+def update_user_query(data_keys):
+    updated_fields = ""
+    count_keys = len(data_keys)
+    for i, key in enumerate(data_keys):
+        updated_fields += f"{key}=(:{key})"
+        if i < count_keys-1:
+            updated_fields += ", "
+    return text(f"""
+        update users
+        set {updated_fields}
+        where id=(:id)
+    """)
+
+
+def delete_user_query():
+    return text(f"delete from users where id=(:id)")
 
 
 async def add_default_users(db):
@@ -21,10 +40,10 @@ async def add_default_users(db):
         "Smokey", "Annie", "Tigger",
         "Bella", "Midnight", "Cookie",
         "Luna", "Sheba", "Oreo",
-        "Peanut", "Oliver", "Molly"]
+        "Peanut", "Oliver", "Molly"
+    ]
     async with db.begin() as conn:
-        await conn.execute(text(
-            """
+        await conn.execute(text("""
             create table if not exists users 
             (
             id serial primary key,
@@ -35,9 +54,8 @@ async def add_default_users(db):
             country varchar,
             city varchar
             )
-            """
-        ))
-        cursor = await conn.execute(select_all_users_query)
+        """))
+        cursor = await conn.execute(select_all_users_query())
         if not len(cursor.all()):
             for i in range(40):
                 avatar_name = avatars[randint(0, 11)]
@@ -49,27 +67,31 @@ async def add_default_users(db):
                     "country": fake.country(),
                     "city": fake.city(),
                 }
-                await conn.execute(insert_user_query, user_data)
+                await conn.execute(insert_user_query(), user_data)
         await conn.commit()
     await db.dispose()
 
 
 async def select_all_users(db):
     async with db.begin() as conn:
-        cursor = await conn.execute(select_all_users_query)
+        cursor = await conn.execute(select_all_users_query())
     await db.dispose()
     return cursor.all()  # TODO: if users a lot then do pagination
 
 
 async def create_new_user(db, user_data):
     async with db.begin() as conn:
-        await conn.execute(insert_user_query, user_data)
+        await conn.execute(insert_user_query(), user_data)
     await db.dispose()
 
 
 async def update_user(db, user_data):
-    pass
+    async with db.begin() as conn:
+        await conn.execute(update_user_query(user_data.keys()), user_data)
+    await db.dispose()
 
 
-async def delete_user(db, id):
-    pass
+async def delete_user(db, user_data):
+    async with db.begin() as conn:
+        await conn.execute(delete_user_query(), user_data)
+    await db.dispose()
--- handlers/handlers.py
+++ handlers/handlers.py
@@ -6,22 +6,32 @@ from database.users_managment import select_all_users, create_new_user, update_u
 
 
 async def users_handler(request):
-    match request.method:
-        case "GET":
-            users = await select_all_users(database_engine)
-
-            # Convert Row objects(from database) to dicts
-            users = [user._asdict() for user in users]  # TODO: not good
-            users = {
-                "users": users
-            }
-            users_json = json.dumps(users)
-            return web.json_response(users_json, headers={"Access-Control-Allow-Origin": "*"})
-        case "POST":
-            new_user = await request.json()  # dict
-            await create_new_user(database_engine, new_user)
-            return web.Response(text=f"Good ?")
-        case "PUT":
-            return web.Response(text=f"{dir(request)}")
-        case "DELETE":
-            return web.Response(text=f"{dir(request)}")
+    try:
+        match request.method:
+            case "GET":
+                users = await select_all_users(database_engine)
+                # Convert Row objects(from database) to dicts
+                users = [user._asdict() for user in users]  # TODO: not good
+                users = {
+                    "success": True,
+                    "message": f"Users list",
+                    "users": users
+                }
+                users_json = json.dumps(users)
+                return web.json_response(users_json, headers={"Access-Control-Allow-Origin": "*"})
+            case "POST":
+                new_user = await request.json()  # dict
+                await create_new_user(database_engine, new_user)
+                return web.json_response({"success": True, "message": f"User created"}, status=200)
+            case "PUT":
+                updated_user = await request.json()
+                await update_user(database_engine, updated_user)
+                return web.json_response({"success": True, "message": f"User updated"}, status=200)
+            case "DELETE":
+                deleted_user = await request.json()
+                await delete_user(database_engine, deleted_user)
+                return web.json_response({"success": True, "message": f"User deleted"}, status=200)
+            case _:
+                return web.json_response({"success": False, "message": f"This method not available"}, status=404)
+    except Exception as ex:
+        return web.json_response({"success": False, "message": f"Error: {ex}"}, status=500)
