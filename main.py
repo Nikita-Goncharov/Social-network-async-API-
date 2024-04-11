920c981a255c0db2fdc1071a37bc81a264a76a2f
Nikita Goncharov <nikitagoncarov657@gmail.com>
2024-04-11 20:31:59 +0300

    add urls for auth

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from aiohttp import web
 
 from handlers.profile_handler import profile_handler
 from handlers.service_handler import github_pull_updates, api_docs
-from handlers.auth_handler import logout_handler
+from handlers.auth_handler import logout_handler, login_handler, register_handler
 from handlers.post_handler import post_handler
 from database.base_orm import add_default_data
 
 
+app = web.Application()
+
 if __name__ == "__main__":
-    app = web.Application()
     app.add_routes([
         web.get("/", api_docs),
-        # web.route("*", "/api/v0.1/users", user_handler),
+        web.route("*", "/api/v0.2/register", register_handler),
+        web.route("*", "/api/v0.2/login", login_handler),
+        web.route("*", "/api/v0.2/logout", logout_handler),
+
         web.route("*", "/api/v0.2/profiles", profile_handler),
         web.post("/api/v0.2/pull_repository_changes", github_pull_updates),
-        web.route("*", "/api/v0.2/logout", logout_handler),
         web.route("*", "/api/v0.2/posts", post_handler),
         # web.route("*", "/api/v0.1/dialogs", dialogs_handler),
         # web.route("*", "/api/v0.1/messages", messages_handler),
