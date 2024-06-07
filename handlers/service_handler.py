d3957835451b1d2ac9a8a7728ecf1087226c2484
Nikita Goncharov <nikitagoncarov657@gmail.com>
2024-06-07 10:16:24 +0300

    update webhook receiving

--- handlers/service_handler.py
+++ handlers/service_handler.py
@@ -8,36 +8,39 @@ from utils.generate_github_signature import generate_github_signature
 
 
 async def github_pull_updates(request: web.Request) -> web.Response:
-    github_signature = request.headers.get("X-Hub-Signature")
-    payload = await request.json()
-    GITHUB_HOOK_SECRET = os.environ.get("GITHUB_HOOK_SECRET")
-
-    print("Signature header from request:", github_signature)
-    print("Secret from config:", GITHUB_HOOK_SECRET)
-    print("Request body:", payload)
-
-    generated_signature = generate_github_signature(GITHUB_HOOK_SECRET.encode("utf-8"), payload)
-    if github_signature != generated_signature:
-        print("X-Hub-Signature is incorrect")
-        print("Generated signature is:", generated_signature)
-        return web.Response(text="Error. Secret keys are not the same", status=403)
-
-    repo = Repo()
-    repo.git.stash()
-    origin = repo.remote("origin")
-    origin.pull("develop")
-    # repo.remotes.origin.fetch()
-    # develop_branch = repo.remote().refs['develop']
-    # repo.git.merge(develop_branch)
-    repo.git.stash("pop")
-
     try:
+        github_signature = request.headers.get("X-Hub-Signature")
+        payload = await request.json()
+        GITHUB_HOOK_SECRET = os.environ.get("GITHUB_HOOK_SECRET")
+
+        print("Signature header from request:", github_signature)
+        print("Secret from config:", GITHUB_HOOK_SECRET)
+        print("Request body:", payload)
+
+        generated_signature = generate_github_signature(GITHUB_HOOK_SECRET.encode("utf-8"), payload)
+        if github_signature != generated_signature:
+            print("X-Hub-Signature is incorrect")
+            print("Generated signature is:", generated_signature)
+            return web.Response(text="Error. Secret keys are not the same", status=403)
+
+        repo = Repo()
+        repo.git.stash()
+        origin = repo.remote("origin")
+        origin.pull("develop")
+        # repo.remotes.origin.fetch()
+        # develop_branch = repo.remote().refs['develop']
+        # repo.git.merge(develop_branch)
+        repo.git.stash("pop")
+
+        # try:
         subprocess.run("sudo systemctl reload nginx", check=True)  # sudo supervisorctl reread & sudo supervisorctl update
-    except subprocess.CalledProcessError as ex:
-        print("Can`t reload site")
+        # except subprocess.CalledProcessError as ex:
+        #     print("Can`t reload site")
+        #     return web.Response(text=f"Error reloading application: {ex}", status=500)
+        print("Webhook received and application reloaded successfully")
+        return web.Response(text="Webhook received and application reloaded successfully", status=200)
+    except Exception as ex:
         return web.Response(text=f"Error reloading application: {ex}", status=500)
-    print("Webhook received and application reloaded successfully")
-    return web.Response(text="Webhook received and application reloaded successfully", status=200)
 
 
 async def api_docs(request: web.Request) -> web.Response:
